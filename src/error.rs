e84bab2f6d47de45935fdf2f47856f769d0d2ef3
benjarison <benjamin.jt.harrison@gmail.com>
2023-07-25 18:05:23 -0400

    Add documentation to PcaError

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,48 @@
 
+///
+/// Defines various errors
+/// 
 #[derive(Clone, Debug)]
 pub enum PcaError {
+    /// Indicates a failure encountered during Kernel PCA computation
     ComputationFailure(String),
+    /// Indicates an invalid Kernel PCA configuration
     InvalidConfig(String),
+    /// Indicates invalid input data
     InvalidData(String)
 }
 
 impl PcaError {
 
+    ///
+    /// Constructs a new ComputationFailure instance
+    /// 
+    /// # Arguments
+    /// 
+    /// * `message` - The error message
+    /// 
     pub fn computation_failure(message: impl Into<String>) -> PcaError {
         PcaError::ComputationFailure(message.into())
     }
 
+    ///
+    /// Constructs a new InvalidConfig instance
+    /// 
+    /// # Arguments
+    /// 
+    /// * `message` - The error message
+    /// 
     pub fn invalid_config(message: impl Into<String>) -> PcaError {
         PcaError::InvalidConfig(message.into())
     }
 
+    ///
+    /// Constructs a new InvalidData instance
+    /// 
+    /// # Arguments
+    /// 
+    /// * `message` - The error message
+    /// 
     pub fn invalid_data(message: impl Into<String>) -> PcaError {
         PcaError::InvalidData(message.into())
     }
